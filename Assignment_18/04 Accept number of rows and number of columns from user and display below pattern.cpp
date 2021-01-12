f284215adfae8c618648ad1c8732557212a59d3b
snehal renushe <snehalrenushe15@gmail.com>
2021-01-12 22:56:28 +0530

    Eighteenth Assignment

---
+++ Assignment_18/04 Accept number of rows and number of columns from user and display below pattern.cpp
@@ -0,0 +1,44 @@
+/*
+   Input  : iRow = 6     iCol = 5
+   Output : *  *  *  *  *
+            *  @  @  @  *
+            *  @  @  @  *
+            *  @  @  @  *
+            *  @  @  @  *
+            *  *  *  *  *
+*/
+
+///============================================================///
+
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    for(int i = 1 ; i <= iRow ; i++)
+    {
+        for(int j = 1 ; j <= iCol ; j++)
+        {
+            if(i == 1 || i == iRow || j == 1 || j == iCol)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf(" @ ");
+            }
+        }
+
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+
+    printf("\nEnter number of rows and columns elements => ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
