cf5a98a114dfe39d8bffea4d3b2cea44cfaee0d9
snehal renushe <snehalrenushe15@gmail.com>
2021-01-14 23:18:24 +0530

    Twenth one Assignment

---
+++ Assignment_21/04 Accept N numbers from user and return difference between summation of even elements and summation of odd elements.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int Display(int Arr[],int iLength)
+{
+    for(int i = 0 ; i < iLength ; i++)
+    {
+        if(Arr[i] % 5 == 0 && Arr[i] % 3 == 0)
+        {
+            printf("%d ",Arr[i]);
+        }
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int iSize = 0,iRet = 0,iCnt = 0;
+    int *p = NULL;
+
+    printf("\n Enter number of elements : ");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("\n Unable to allocate memory ...");
+        return -1;
+    }
+
+    printf("\n Enter %d elements ",iSize);
+
+    for(iCnt = 0 ; iCnt < iSize ; iCnt++)
+    {
+        printf("\n Enter elements %d : ",iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Display(p,iSize);
+
+    free(p);
+
+    return 0;
+}
