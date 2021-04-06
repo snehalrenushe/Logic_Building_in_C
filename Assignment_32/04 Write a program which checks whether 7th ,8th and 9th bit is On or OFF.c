c045a1ad8c848948a65382caf526013bf1b6f294
snehal renushe <snehalrenushe15@gmail.com>
2021-04-06 21:57:12 +0530

    Thirty two Assignment

---
+++ Assignment_32/04 Write a program which checks whether 7th ,8th and 9th bit is On or OFF.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+typedef int BOOL;
+typedef unsigned int UINT;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkBit(UINT N)
+{
+    BOOL flag = 0;
+
+    if((N & (1 << 6)) && (N & (1 << 7)) && (N & (1 << 8)))
+    {
+        flag = 1;
+    }
+
+    return flag;
+}
+
+int main()
+{
+    UINT No = 0;
+    BOOL Ret = 0;
+
+    printf("\nEnter a number => ");
+    scanf("%d",&No);
+
+    Ret = ChkBit(No);
+
+    if(Ret)
+    {
+        printf("\n15th Bit of Given number %d is ON.",No);
+    }
+    else
+    {
+        printf("\n15th Bit of Given number %d is OFF.",No);
+    }
+
+    getch();
+    return 0;
+}
