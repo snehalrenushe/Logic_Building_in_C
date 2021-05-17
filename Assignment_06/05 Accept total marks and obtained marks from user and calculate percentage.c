f892c60e3f0767e2d7c1159f31ae16a45eeccfe7
snehal renushe <snehalrenushe15@gmail.com>
2021-05-17 18:01:54 +0530

    Sixth Assignment

--- Assignment_06/05 Accept total marks and obtained marks from user and calculate percentage.c
+++ Assignment_06/05 Accept total marks and obtained marks from user and calculate percentage.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 
-float Percentage(float Tmarks,float Omarks)
+float Percentage(int *Tmarks,int *Omarks)
 {
-    float per;
+    if(Tmarks < Omarks || Tmarks < 0)
+    {
+        return -1;
+    }
 
-    per = (Omarks / Tmarks) * 100.0;
-
-    return per;
+    return ( (float)(*Omarks)/(*Tmarks) )* (100);
+    //return ((float)Omarks / Tmarks) * (100);
 
 }
+
 int main()
 {
     int iValue1 = 0,iValue2 = 0;
@@ -20,9 +23,16 @@ int main()
     printf("\nPlease enter obtained marks : ");
     scanf("%d",&iValue2);
 
-    fRet = Percentage(iValue1,iValue2);
+    fRet = Percentage(&iValue1,&iValue2);
 
-    printf(" %.2f% ",fRet);
+    if(fRet == -1)
+    {
+        printf("\nInvalid Input.....\n\n");
+    }
+    else
+    {
+        printf("\n Percentage is : %0.2f% \n\n",fRet);
+    }
 
     return 0;
 }
