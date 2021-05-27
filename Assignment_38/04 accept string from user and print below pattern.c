9b6b4852a86874583f818269a1379d3124caa3ba
snehal renushe <snehalrenushe15@gmail.com>
2021-05-27 20:54:32 +0530

    Thirty Eighth Assignment

---
+++ Assignment_38/04 accept string from user and print below pattern.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(char *pstr)
+{
+    int i = 0 , j = 0 , Temp = 0;
+
+    Temp = strlen(pstr);
+
+    printf("\n");
+
+    for(i = 0 ; i < Temp ; i++)
+    {
+        for(j = 0 ; j < Temp ; j++)
+        {
+            if(pstr[j] >= 'a' && pstr[j] <= 'z')
+            {
+                printf("%c ",pstr[j]-32);
+            }
+            else
+            {
+                printf("%c ",pstr[j]);
+            }
+        }
+        printf("\n");
+    }
+    return;
+}
+
+int main()
+{
+    char str[30]="\0";
+
+    printf("Enter The String : ");
+    scanf(" %[^\n]s",str);
+
+    Pattern(str);
+
+    printf("%s",str);
+    return 0;
+}
