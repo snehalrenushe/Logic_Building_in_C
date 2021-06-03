4e6a70525c2f209aeeba5d8fddff847f8fbc20e8
snehal renushe <snehalrenushe15@gmail.com>
2021-06-03 20:22:30 +0530

    Fortyth Assignment

---
+++ Assignment_40/05 program which number from user and display below pattern..c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(int iNo)
+{
+    int Rev = 0;
+
+    for(int i = iNo ; i % 10 > 0 ; i /= 10)
+    {
+        for(int j = iNo ; j % 10 != (i / 10) % 10 ; j /= 10)
+        {
+            printf(" %d ",j%10);
+        }
+
+        printf("\n");
+    }
+    return;
+}
+int main()
+{
+    int No = 0;
+
+    printf("Enter a number => ");
+    scanf("%d",&No);
+
+    Pattern(No);
+
+    return 0;
+}
